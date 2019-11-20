142a3de287e279d616a9eb307e0ee5744c332bf8
KayAllTheWay <kenitagarffus1@gmail.com>
2019-11-20 13:20:18 +0000

    added rspec gem to test application and solve issue with simple form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,41 +1,55 @@
-class JobsController < ApplicationController 
-    before_action : find_job, only: [:show, :edit, :update, :destroy]
-    def index
-    end
+class JobsController < ApplicationController
+	before_action :find_job, only: [:show, :edit, :update, :destroy]
 
-  def show
-  end  
+	def index
+		if params[:category].blank?
+			@jobs = Job.all.order("created_at DESC")
+		else
+			@category_id = Category.find_by(name: params[:category]).id
+			@jobs = Job.where(category_id: @category_id).order("created_at DESC")
+		end
+	end
 
- def new 
-    @job = Job.new
- end
- 
- def create 
-    @job = Job.new(jobs_params)
+	def show
+	end
 
-    if @jobs.save
-        redirect_to @job  
-    else 
-       render  "New"
-    end 
- end
+	def new
+		@job = Job.new
+	end
 
- def edit 
- end 
+	def create
+		@job = Job.new(jobs_params)
 
- def update
- end 
+		if @job.save
+			redirect_to @job
+		else
+			render "New"
+		end
+	end
 
- def destroy 
- end 
+	def edit
+	end
 
- private
+	def update
+		if @job.update(jobs_params)
+			redirect_to @job
+		else
+			render "Edit"
+		end
+	end
 
- def jobs_params
-    params.require(:job).permit(:title, :school, :salary, :description)
- end 
+	def destroy
+		@job.destroy
+		redirect_to root_path
+	end
 
-  def find_job 
-    @job = Job.find(params[:id])
-  end 
+	private
+
+	def jobs_params
+		params.require(:job).permit(:title, :school, :salary, :description,)
+	end
+
+	def find_job
+		@job = Job.find(params[:id])
+	end
 end
